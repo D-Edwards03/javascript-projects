6daf3db9a56f39e197b45b13b4e7c22bdc45e476
Demetrius Edwards <edwardsd03@outlook.com>
2024-06-20 20:50:49 -0500

    class 3 studio strings and arrays

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -4,8 +4,28 @@ let str = "LaunchCode";
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
+//let newStr = str.slice(3) + str.slice(0, 3);
+//console.log(newStr);
+
 //Use a template literal to print the original and modified string in a descriptive phrase.
+
+//console.log(`'we change ${str} into ${newStr} using string methods.`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
+let numLetters = Number(input.question("How many letters will be relocated?"));
+
+let numStr = str.slice(numLetters) + str.slice(0, numLetters);
+
+let defaultStr = str.slice(3) + str.slice(0, 3);
+
+//console.log(numStr);
+
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+if (numLetters > str.length) {
+    console.log("input exceeds length of the word.");
+
+    console.log(`'We change ${str} into ${defaultStr} using string methods'`);
+} else {
+    console.log(`'We change ${str} into ${numStr} using string methods'`);
+}
