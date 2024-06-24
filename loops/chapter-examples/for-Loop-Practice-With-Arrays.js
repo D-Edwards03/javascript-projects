eb122bb9c32d4a4e1ec98e8e500ea1aea4de2801
Demetrius Edwards <edwardsd03@outlook.com>
2024-06-24 16:29:21 -0500

    ch.9 exercises complete

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,9 @@
 // create an array variable containing the names
 
+let familyNames = ['Demetrius', 'Dior', 'Demi'];
+
 // write a for loop that prints each name on a different line
+
+for (i = 0; i < familyNames.length; i++) {
+    console.log(familyNames[i]);
+}
--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,9 @@
 // Create a string variable containing your name.
 
+let firstName = 'Demetrius';
 
 // Write a for loop that prints each character in your name on a different line.
+
+for ( let i = 0; i < firstName.length; i++) {
+    console.log(firstName[i]);
+}
