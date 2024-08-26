83cfc8141aee7493a4bed303e03dc12c79901c91
Demetrius Edwards <edwardsd03@outlook.com>
2024-08-26 04:04:14 -0500

    class 15 / Ch.25 Form

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,27 @@
 //Code Your Solution Below
+function init () {
+    const form = document.querySelector('form');
+    const inputs = form.querySelectorAll('input, select');
+    const paragraph = document.getElementById("statusReport");
+
+    form.addEventListener('submit', function(event) {
+        let allFilled = true;
+
+        inputs.forEach(input => {
+            if (input.type != 'checkbox' && input.value === '') {
+                allFilled = false;
+            }
+            if (input.type === 'checkbox' && !input.checked) {
+                allFilled = false;
+            }
+        });
+
+        //prevents form submisiion
+        if (!allFilled) {
+            event.preventDefault();
+            paragraph.innerHTML = 'Please fill out all fields.'
+        }
+    });
+}
+
+window.addEventListener("load", init);
